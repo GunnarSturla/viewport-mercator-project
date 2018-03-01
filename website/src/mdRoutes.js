e52586e0a88abae1f63b826a8f5919f6669b37ac
Ib Green <ib@uber.com>
2018-03-01 14:05:43 -0800

    Update ocular scaffold

--- website/src/mdRoutes.js
+++ website/src/mdRoutes.js
@@ -7,25 +7,25 @@ export default [{
       markdown: require('../../docs/README.md')
     }, {
       name: 'What\'s New',
-      markdown: require('../../docs/get-started/whats-new.md')
+      markdown: require('../../docs/whats-new.md')
     }]
   }, {
     name: 'Get started',
     children: [{
-      name: 'Installation',
-      markdown: require('../../docs/get-started/README.md')
-    }, {
       name: 'About Coordinates',
       markdown: require('../../docs/get-started/coordinates.md')
+    }, {
+      name: 'Offset Accuracy',
+      markdown: require('../../docs/get-started/offset-accuracy.md')
     }]
   }, {
     name: 'API Reference',
     children: [{
-      name: 'PerspectiveMercatorViewport',
-      markdown: require('../../docs/api-reference/perspective-mercator-viewport.md')
+      name: 'WebMercatorViewport',
+      markdown: require('../../docs/api-reference/web-mercator-viewport.md')
     }, {
-      name: 'FlatMercatorViewport',
-      markdown: require('../../docs/api-reference/flat-mercator-viewport.md')
+      name: 'Web Mercator Utils',
+      markdown: require('../../docs/api-reference/web-mercator-utils.md')
     }]
   }]
 }];
